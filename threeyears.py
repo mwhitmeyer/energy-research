42353328691bd17ce6f7d434e50455c720f5121f
Michael Whitmeyer <mwhitmeyer@berkeley.edu>
2017-08-29 17:58:27 -0700

    finalized

--- threeyears.py
+++ threeyears.py
@@ -9,6 +9,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
+import time
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+initialtime = current_milli_time()
 
 threeyears = pd.read_csv('12012013-08312016thirtyhomes1.csv')
 
@@ -17,7 +22,7 @@ threeyears['localminute'] = [row[1][:len(row[1]) - 3] for row in threeyears.iter
 
 threeyears['date'], threeyears['time'] = threeyears['localminute'].str.split(' ', 1).str
 
-threeyears['localminute'] = [pd.to_datetime(row[1]) for row in threeyears.itertuples()]
+#threeyears['localminute'] = [pd.to_datetime(row[1]) for row in threeyears.itertuples()]
 
 gbDate = threeyears.groupby(['date', 'time']).sum()
 
@@ -120,6 +125,38 @@ def getSummerGen():
     summer3 = gbDate.loc['2016-06-01':'2016-08-30', ['use', 'gen']]
     summer3 = summer3.groupby(['time', 'date']).sum()
     
+    hours = set()
+    count = 0
+    for row in gbDate.iterrows():
+        hours.add(row[0][1])
+        if count == 24:
+            break;
+        count += 1
+    hours = list(hours)
+    hours.sort()
+    
+    toWrite = pd.DataFrame()
+    maxUseOrGen = max(gbDate['gen'].max(), gbDate['use'].max())
+    evenspaced = np.arange(0, maxUseOrGen + 1, 1)
+    toWrite['power'] = evenspaced
+    
+    for hour, k in zip(hours[6:21], range(6, 21)):
+        plt.figure(figsize=(12,8)) 
+        x1 = summer1.loc[hour]['gen']
+        x2 = summer2.loc[hour]['gen']
+        x3 = summer3.loc[hour]['gen']
+        combined = x1.append(x2).append(x3)
+        kde = stats.gaussian_kde(combined, 0.35)
+        pdf = kde.pdf(evenspaced)
+        toWrite[str(k) + ':00 pdf'] = pdf
+        plt.axvline(np.percentile(combined, 95), linestyle = '--', color = 'r')
+        plt.plot(evenspaced, pdf)
+        plt.hist(combined, normed = True)
+        plt.ylabel('Probability Density')
+        plt.title('Summer Generation Histogram for ' + str(k) + ':00')
+    
+    toWrite.to_csv('threesummergen.csv')
+    
     
 def getSummerLoad():
     summer1 = gbDate.loc['2014-06-01':'2014-08-30', ['use', 'gen']]
@@ -128,5 +165,38 @@ def getSummerLoad():
     summer2 = summer2.groupby(['time', 'date']).sum()
     summer3 = gbDate.loc['2016-06-01':'2016-08-30', ['use', 'gen']]
     summer3 = summer3.groupby(['time', 'date']).sum()
+    
+    hours = set()
+    count = 0
+    for row in gbDate.iterrows():
+        hours.add(row[0][1])
+        if count == 24:
+            break;
+        count += 1
+    hours = list(hours)
+    hours.sort()
+    
+    toWrite = pd.DataFrame()
+    maxUseOrGen = max(gbDate['gen'].max(), gbDate['use'].max())
+    evenspaced = np.arange(0, maxUseOrGen + 1, 1)
+    toWrite['power'] = evenspaced
+    
+    for hour, k in zip(hours, range(24)):
+        plt.figure(figsize=(12,8)) 
+        x1 = summer1.loc[hour]['use']
+        x2 = summer2.loc[hour]['use']
+        x3 = summer3.loc[hour]['use']
+        combined = x1.append(x2).append(x3)
+        kde = stats.gaussian_kde(combined, 0.35)
+        pdf = kde.pdf(evenspaced)
+        toWrite[str(k) + ':00 pdf'] = pdf
+        plt.axvline(np.percentile(combined, 95), linestyle = '--', color = 'r')
+        plt.plot(evenspaced, pdf)
+        plt.hist(combined, normed = True)
+        plt.ylabel('Probability Density')
+        plt.title('Summer Load Histogram for ' + str(k) + ':00')
+    
+    toWrite.to_csv('threesummeruse.csv')
 
-
+finaltime = current_milli_time()
+print('time taken in milliseconds: ' + str(finaltime-initialtime))
