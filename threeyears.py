97fa9c0806096974fd61128dccef7e5a9d181738
Michael Whitmeyer <mwhitmeyer@berkeley.edu>
2017-09-03 23:23:18 -0700

    commented

--- threeyears.py
+++ threeyears.py
@@ -17,13 +17,16 @@ initialtime = current_milli_time()
 
 threeyears = pd.read_csv('12012013-08312016thirtyhomes1.csv')
 
-#get rid of timezone at the end of the localminute obj 
+#get rid of timezone at the end of the localminute obj ('00:00:00-05' --> '00:00:00')
 threeyears['localminute'] = [row[1][:len(row[1]) - 3] for row in threeyears.itertuples()] 
 
 threeyears['date'], threeyears['time'] = threeyears['localminute'].str.split(' ', 1).str
 
 #threeyears['localminute'] = [pd.to_datetime(row[1]) for row in threeyears.itertuples()]
 
+
+# group by date and time, then sum. 
+# Lets us easily choose the date range, and then group by time and date
 gbDate = threeyears.groupby(['date', 'time']).sum()
 
 
@@ -34,6 +37,7 @@ def getAllWinterData():
 
     
 def getWinterGen():
+    # get the three winters, then group by the hour
     winter1 = gbDate.loc['2013-12-01':'2014-02-28', ['use', 'gen']]
     winter1 = winter1.groupby(['time', 'date']).sum()
     winter2 = gbDate.loc['2014-12-01':'2015-02-28', ['use', 'gen']]
@@ -41,6 +45,7 @@ def getWinterGen():
     winter3 = gbDate.loc['2015-12-01':'2016-02-28', ['use', 'gen']]
     winter3 = winter3.groupby(['time', 'date']).sum()
     
+    # make a sorted list of hours that we iterate through later in the for loop
     hours = set()
     count = 0
     for row in gbDate.iterrows():
@@ -51,16 +56,22 @@ def getWinterGen():
     hours = list(hours)
     hours.sort()
     
+    # initiate the dataframe that we will write to the csv
     toWrite = pd.DataFrame()
+    # find the max power value for our PDF and histograms
     maxUseOrGen = max(gbDate['gen'].max(), gbDate['use'].max())
+    # for our PDF, with step size of 1
     evenspaced = np.arange(0, maxUseOrGen + 1, 1)
+    # the first column of our data frame with the evenly spaced power values
     toWrite['power'] = evenspaced
     
+    # plots a histogram and a pdf for the KDE, and puts the pdf value in toWrite 
     for hour, k in zip(hours[7:20], range(7, 20)):
         plt.figure(figsize=(12,8)) 
         x1 = winter1.loc[hour]['gen']
         x2 = winter2.loc[hour]['gen']
         x3 = winter3.loc[hour]['gen']
+        #combine the three winters of data
         combined = x1.append(x2).append(x3)
         kde = stats.gaussian_kde(combined, 0.35)
         pdf = kde.pdf(evenspaced)
